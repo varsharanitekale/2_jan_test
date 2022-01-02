d3032a7e9d1ed61e1699af7a50feaaf2a393c358
varsharanitekale <varsharanitekale50@gmail.com>
2022-01-02 18:50:13 +0530

    initial first

---
+++ index.js
@@ -0,0 +1,8 @@
+function fun(img) {
+    var img = document.getElementsByClassName('img');
+    img.style.width = "500px";
+    img.style.height = "500px";
+    
+    return img;
+  }
+  console.log(fun());
